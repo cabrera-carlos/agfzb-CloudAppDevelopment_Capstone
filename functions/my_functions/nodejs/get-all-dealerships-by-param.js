6c107738758664d38fb48e33f2962b5d79506b0d
cabrera-carlos <cabrera.crcc@gmail.com>
2022-03-25 15:56:00 -0400

    Add dealerId to get-dealers Cloud Func

--- functions/my_functions/nodejs/get-all-dealerships-by-param.js
+++ functions/my_functions/nodejs/get-all-dealerships-by-param.js
@@ -4,18 +4,19 @@
 const Cloudant = require('@cloudant/cloudant');
 
 async function main(params) {
-    const state_check = "state" in params;
+    // Create a Cloudant client
     const cloudant = Cloudant({
         url: params.COUCH_URL,
         plugins: { iamauth: { iamApiKey: params.IAM_API_KEY } }
     });
 
     try {
+        // Get the car-dealership database
         const dealerships_db = cloudant.use('dealerships')
 
-        // Check if "state" parameter has been provided
-        // If so, then query by "state"
-        if (state_check && params.state != "") {
+        // Check if "state" or "dealerId" parameters have been provided
+        // If so, then query by parameter
+        if ("state" in params && params.state != "") {
             const query = {
                 selector: {
                     st: params.state
@@ -23,20 +24,21 @@ async function main(params) {
             }
 
             const response = await dealerships_db.find(query);
+            return response
 
+        } else if ("dealerId" in params && params.dealerId != "") {
+            const query = {
+                selector: {
+                    id: Number(params.dealerId)
+                }
+            }
+
+            const response = await dealerships_db.find(query);
             return response
         }
 
         // Else return all documents
         const doclist = await dealerships_db.list({ include_docs: true })
-
-        // let records = [];
-
-        // for (let i = 0; i < doclist.total_rows; i++) {
-        //     records.push(doclist.rows[i].doc);
-        // }
-
-        // return { "dealerships": records };
         return doclist
 
     } catch (error) {
